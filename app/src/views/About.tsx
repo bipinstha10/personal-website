147935bb6c7b9fd15abfc7dbd929b295079aef9a
Bipin <beepeenstha10@gmail.com>
2025-09-25 20:01:41 +0545

    feat: improve styling and layout

--- app/src/views/About.tsx
+++ app/src/views/About.tsx
@@ -2,29 +2,41 @@ import Gallery from "./Gallery";
 
 const About = () => {
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex-1 flex justify-center items-center">
       <div className="w-[80%] md:w-[65%]">
         <h1 className="md:text-8xl text-5xl font-display font-bold mb-8 mt-10 text-center">
           I'm Bipin.
         </h1>
-        <p className="text-center text-md/5">
-          Hi, I’m Bipin Shrestha, <br />a third-year Electronics, Communication,
-          and Information Engineering student at Pulchowk Campus.
-          <br />
-          I’m currently exploring my journey into full-stack web development,
-          building small projects to sharpen my skills. My goal is to grow as a
-          developer and create meaningful applications that combine design and
-          functionality.
-          <br />
-          Outside of coding, I have a passion for photography. It started as a
-          hobby and is still one of my favorite ways to capture memories.
-          <br />
-          When I’m not studying or coding, you’ll probably find me playing
-          football, strumming the guitar, or enjoying a good anime series.
-          <br />
-          Thanks for stopping by. Feel free to check out my gallery, explore my
-          projects, or just reach out to connect!
-        </p>
+        <div className="grid grid-cols-1 md:grid-cols-2 md:px-10">
+          <div className="w-[300px] h-[350px] md:w-[400px] md:h-[450px] bg-red-950 rounded-t-full relative overflow-hidden">
+            <img
+              src="/images/Bipin.png"
+              alt=""
+              className="scale-130 absolute bottom-[-100px] left-5"
+            />
+          </div>
+          <div>
+            <p className="text-md text-gray-700 dark:text-gray-400 mt-8 md:mt-0 md:text-xl leading-8">
+              an Electronics, Communication, and Information Engineering student
+              at Pulchowk Campus.
+              <br />
+              I’m currently exploring my journey into full-stack web
+              development, building small projects to sharpen my skills. My goal
+              is to grow as a developer and create meaningful applications that
+              combine design and functionality.
+              <br />
+              Outside of coding, I have a passion for photography. It started as
+              a hobby and is still one of my favorite ways to capture memories.
+              <br />
+              When I’m not studying or coding, you’ll probably find me playing
+              football, strumming the guitar, or enjoying a good anime series.
+              <br />
+              Thanks for stopping by. Feel free to check out my gallery, explore
+              my projects, or just reach out to connect!
+            </p>
+          </div>
+        </div>
+
         <h1 className="text-2xl md:text-3xl font-bold mb-8 mt-10 text-center">
           My Photography
         </h1>
--- app/src/views/Home.tsx
+++ app/src/views/Home.tsx
@@ -3,8 +3,10 @@ const Home = () => {
     <div className="flex-1 flex justify-center items-center overflow-hidden">
       <div className="md:w-[40%] text-center">
         <div className="flex flex-col justify-center ">
-          <div className="relative w-full h-[100px]">
-            <span className="sun absolute bg-black dark:bg-white top-0 right-0"></span>
+          <div className="relative w-[full] h-[100px]">
+            <div className="absolute top-5 right-5 md:top-0 md:right-0 w-[60px] h-[60px] md:w-[80px] md:h-[80px]">
+              <div className="sun bg-black dark:bg-white"></div>
+            </div>
           </div>
           <h1 className="md:text-8xl text-5xl font-display font-bold mb-4">
             Hi. I'm Bipin. <br /> A Developer.
@@ -14,13 +16,13 @@ const Home = () => {
             creative projects. I enjoy blending logic with artistry to build
             meaningful work.
           </p>
-          <div className="relative w-full h-[100px]">
-            <div className="moon absolute top-0 left-0">
-              <div className="crescentMoon">
-                <div className="crescent bg-black dark:bg-white"></div>
-                <div className="circle bg-white dark:bg-black"></div>
-                <div className="star bg-black dark:bg-white"></div>
+          <div className="relative w-[100px] h-[100px]">
+            <div className="moon absolute top-0 left-5 md:left-[-5em]">
+              <div className="crescentMoon relative w-[60px] h-[60px] md:w-[80px] md:h-[80px]">
+                <div className="circle  bg-black dark:bg-white"></div>
+                <div className="crescent  bg-white dark:bg-black"></div>
               </div>
+              <div className="star  bg-black dark:bg-white"></div>
             </div>
           </div>
         </div>
